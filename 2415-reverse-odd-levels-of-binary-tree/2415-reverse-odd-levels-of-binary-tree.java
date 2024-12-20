fcda80764fbe4dfbcaaf8d9d445335336fbacb78
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2024-12-20 18:40:17 +0530

    Time: 21 ms (5.72%), Space: 49.6 MB (24.35%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -1,52 +1,52 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        Stack<TreeNode> stack1=new Stack<>();
-        Stack<TreeNode> stack2=new Stack<>();
-        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         stack1.push(root);
-        while(!stack1.isEmpty()||!stack2.isEmpty()){
-            while(!stack1.isEmpty()){
-                TreeNode temp=stack1.pop();
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                TreeNode temp = stack1.pop();
                 list.add(temp.val);
-                if(temp.left!=null)
-                 stack2.push(temp.left);
-                if(temp.right!=null)
-                 stack2.push(temp.right);
+                if (temp.left != null)
+                    stack2.push(temp.left);
+                if (temp.right != null)
+                    stack2.push(temp.right);
             }
-            while(!stack2.isEmpty()){
-                TreeNode temp=stack2.pop();
+            while (!stack2.isEmpty()) {
+                TreeNode temp = stack2.pop();
                 list.add(temp.val);
-                if(temp.right!=null)
-                 stack1.push(temp.right);
-                if(temp.left!=null)
-                 stack1.push(temp.left);
+                if (temp.right != null)
+                    stack1.push(temp.right);
+                if (temp.left != null)
+                    stack1.push(temp.left);
             }
         }
-        int i=0;
-        Queue<TreeNode> q=new LinkedList<>();
+        int i = 0;
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
-            TreeNode temp=q.poll();
-            temp.val=list.get(i++);
-            if(temp.left!=null)
-              q.add(temp.left);
-            if(temp.right!=null)
-              q.add(temp.right);
+        while (!q.isEmpty()) {
+            TreeNode temp = q.poll();
+            temp.val = list.get(i++);
+            if (temp.left != null)
+                q.add(temp.left);
+            if (temp.right != null)
+                q.add(temp.right);
         }
         return root;
     }
