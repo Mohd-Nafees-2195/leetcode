f4ac242c729104ceb179e6ef683acc88ed02141d
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2024-12-20 18:40:35 +0530

    Time: 1 ms (72.88%), Space: 47.9 MB (93.54%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -15,39 +15,18 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        stack1.push(root);
-        while (!stack1.isEmpty() || !stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
-                TreeNode temp = stack1.pop();
-                list.add(temp.val);
-                if (temp.left != null)
-                    stack2.push(temp.left);
-                if (temp.right != null)
-                    stack2.push(temp.right);
-            }
-            while (!stack2.isEmpty()) {
-                TreeNode temp = stack2.pop();
-                list.add(temp.val);
-                if (temp.right != null)
-                    stack1.push(temp.right);
-                if (temp.left != null)
-                    stack1.push(temp.left);
-            }
-        }
-        int i = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode temp = q.poll();
-            temp.val = list.get(i++);
-            if (temp.left != null)
-                q.add(temp.left);
-            if (temp.right != null)
-                q.add(temp.right);
-        }
+        DFS(root.left,root.right,0);
         return root;
+    }
+    public void DFS(TreeNode left,TreeNode right, int level){
+        if(left==null&&right==null)
+         return;
+        if(level%2==0){
+            int temp=left.val;
+            left.val=right.val;
+            right.val=temp;
+        }
+        DFS(left.left,right.right,level+1);
+        DFS(left.right,right.left,level+1);
     }
 }
