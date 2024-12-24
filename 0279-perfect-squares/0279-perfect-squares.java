259a811bb2a30b67ae914a5bee609b1a73192f9e
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2024-12-24 16:04:04 +0530

    Time: 68 ms (29.37%), Space: 43 MB (43.62%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,12 +1,33 @@
 class Solution {
-    public int numSquares(int n) {
+
+    //Recursive Approach
+     public int numSquares(int n) {
         int[] dp=new int[n+1];
-        for(int i=1;i<=n;i++){
-            dp[i]=i;
-            for(int x=1;x*x<=i;x++){
-                dp[i]=Math.min(dp[i],dp[i-x*x]+1);
-            }
-        } 
+        return minCount(n,dp);
+    }
+    public int minCount(int n,int[] dp){
+        if(n<=0){
+            return 0;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        dp[n]=n;
+        for(int x=1;x*x<=n;x++){
+            dp[n]=Math.min(dp[n],minCount(n-x*x,dp)+1);
+        }
         return dp[n];
     }
+
+    //Iterative Approach
+    // public int numSquares(int n) {
+    //     int[] dp=new int[n+1];
+    //     for(int i=1;i<=n;i++){
+    //         dp[i]=i;
+    //         for(int x=1;x*x<=i;x++){
+    //             dp[i]=Math.min(dp[i],dp[i-x*x]+1);
+    //         }
+    //     } 
+    //     return dp[n];
+    // }
 }
