6397b9795fee9b2d4437d1e16c2a6acd71229e33
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2024-12-24 15:02:05 +0530

    Time: 29 ms (77.44%), Space: 42.7 MB (63.4%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numSquares(int n) {
-        int[] dp=new int[10001];
+        int[] dp=new int[n+1];
         for(int i=1;i<=n;i++){
             dp[i]=i;
             for(int x=1;x*x<=i;x++){
