0b59d725f8d882111949ae73be7edf0c94479f2b
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2024-12-27 09:49:47 +0530

    Time: 44 ms (92.35%), Space: 48.4 MB (89%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -2,20 +2,22 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
+var expect = function (val) {
     return {
-        toBe:(newVal)=>{
-            if(newVal===val)
-              return true;
-            else
-             throw new Error("Not Equal")
-        },
-        notToBe:(newVal)=>{
-            if(newVal!==val)
-              return true;
-            else
-             throw new Error("Equal")
-        }
+        toBe: (newVal) => (newVal === val) ? true : (() => { throw new Error("Not Equal") })(),
+        notToBe: (newVal) => (newVal !== val) ? true : (() => { throw new Error("Equal") })()
+        // toBe: (newVal) => {
+        //     if (newVal === val)
+        //         return true;
+        //     else
+        //         throw new Error("Not Equal")
+        // },
+        // notToBe: (newVal) => {
+        //     if (newVal !== val)
+        //         return true;
+        //     else
+        //         throw new Error("Equal")
+        // }
     }
 };
 
