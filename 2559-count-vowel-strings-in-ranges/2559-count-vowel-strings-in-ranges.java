6ea0dff7de96a35f37b7ccddf62aac82a9310d6b
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-01-02 09:15:26 +0530

    Time: 11 ms (19.59%), Space: 86.2 MB (25.51%) - LeetHub

---
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int[] arr=new int[100001];
+        int n=words.length;
+        Set<Character> vowel=new HashSet<>();
+        vowel.add('a');vowel.add('e');vowel.add('i');vowel.add('o');vowel.add('u');
+        for(int i=0;i<n;i++){
+         if(vowel.contains(words[i].charAt(0))&&vowel.contains(words[i].charAt(words[i].length()-1))){
+            arr[i]=1;
+          }
+          if(i!=0){
+            arr[i]=arr[i]+arr[i-1];
+          }
+        }
+        int[] ans=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            if(queries[i][0]==0){
+                ans[i]=arr[queries[i][1]];
+            }else{
+                ans[i]=arr[queries[i][1]]-arr[queries[i][0]-1];
+            }
+        }
+       return ans;
+    }
+}
