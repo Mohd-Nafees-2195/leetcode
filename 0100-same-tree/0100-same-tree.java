92bcf1a3cdeab0742c6b50e65998dc71eda436b3
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-01-08 15:20:02 +0530

    Time: 0 ms (100%), Space: 41 MB (75.5%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,34 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean no=true;
-        return isSame(p,q,no);
+        boolean no = true;
+        return isSame(p, q, no);
     }
-    public boolean isSame(TreeNode p,TreeNode q,boolean no){
-        if(p==null&&q==null)
-         return true;
-        if(p==null||q==null)
-         return false;
-        if(!no)
-         return false;
-        if(p.val!=q.val){
-            no=false;
+
+    public boolean isSame(TreeNode p, TreeNode q, boolean no) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (!no)
+            return false;
+        if (p.val != q.val) {
+            no = false;
             return false;
         }
-        return isSame(p.left,q.left,no)&&isSame(p.right,q.right,no);
+        return isSame(p.left, q.left, no) && isSame(p.right, q.right, no);
     }
 }
