b3d59893facffb57df9115b33f83213bce0f05b5
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-02-15 21:08:09 +0530

    Time: 150 ms (28.21%), Space: 44.5 MB (44.51%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            String str=Integer.toString(i*i);
+            if(isPunished(0,str,i,0)){
+                ans+=i*i;
+            }
+        }
+        return ans;
+    }
+    public boolean isPunished(int ind,String str,int i,int sum){
+       int n=str.length();
+       if(ind==n)
+        return i==sum;
+       for(int j=ind;j<n;j++){
+         int val=Integer.parseInt(str.substring(ind,j+1));
+         if(isPunished(j+1,str,i,sum+val)){
+            return true;
+         }
+       }
+       return false;
+    }
+}
