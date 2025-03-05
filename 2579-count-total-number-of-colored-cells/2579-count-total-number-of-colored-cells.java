5122ea56b2c8744d43b24da9003f154866028237
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-03-05 07:01:19 +0530

    Time: 13 ms (4.51%), Space: 46.6 MB (5.33%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -2,9 +2,10 @@ class Solution {
     public long coloredCells(int n) {
         return getColoredCell(n);
     }
-    public long getColoredCell(int n){
-        if(n==1)
-         return 1;
-        return 4*(n-1)+getColoredCell(n-1);
+
+    public long getColoredCell(int n) {
+        if (n == 1)
+            return 1;
+        return 4 * (n - 1) + getColoredCell(n - 1);
     }
 }
