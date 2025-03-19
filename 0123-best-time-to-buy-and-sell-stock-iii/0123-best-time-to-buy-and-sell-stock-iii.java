8855748ae02a72ba93d25cb712100c9cfd3e5ad5
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-03-19 22:37:37 +0530

    Time: 57 ms (41.48%), Space: 64.2 MB (31.3%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,32 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][][] dp=new int[n][2][3];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                Arrays.fill(dp[i][j],-1);
+        int[][][] dp=new int[n+1][2][3];
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int limit=1;limit<=2;limit++){
+                    int profit=0;
+                    if(buy==1){
+                        int buyKaro=dp[ind+1][0][limit]-prices[ind];
+                        int skipKaro=dp[ind+1][1][limit];
+                        profit=Math.max(buyKaro,skipKaro);
+                    }else{
+                        int sellKaro=dp[ind+1][1][limit-1]+prices[ind];
+                        int skipKaro=dp[ind+1][0][limit];
+                        profit=Math.max(sellKaro,skipKaro);
+                    }
+                    dp[ind][buy][limit]=profit;
+                }
             }
         }
-        return getMaxProfit(prices,1,0,2,dp);
+        return dp[0][1][2];
         //if buy=1 then buy the stock else sell the stock
-    }
-    public int getMaxProfit(int[] prices,int buy,int ind,int limit,int[][][] dp){
-        if(ind==prices.length)
-         return 0;
-        if(limit==0)
-         return 0;
-        if(dp[ind][buy][limit]!=-1)
-         return dp[ind][buy][limit];
-        int profit=0;
-        if(buy==1){
-            int buyKaro=getMaxProfit(prices,0,ind+1,limit,dp)-prices[ind];
-            int skipKaro=getMaxProfit(prices,1,ind+1,limit,dp);
-            profit=Math.max(buyKaro,skipKaro);
-        }else{
-            int sellKaro=getMaxProfit(prices,1,ind+1,limit-1,dp)+prices[ind];
-            int skipKaro=getMaxProfit(prices,0,ind+1,limit,dp);
-            profit=Math.max(sellKaro,skipKaro);
-        }
-        return dp[ind][buy][limit]=profit;
     }
 }
