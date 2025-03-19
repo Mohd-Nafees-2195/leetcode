2d180ff3dc99be49f6598525bbf9f496292a8243
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-03-19 21:52:31 +0530

    Time: 0 ms (100%), Space: 45.2 MB (99.58%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][] dp=new int[n+1][2];
+        //int[][] dp=new int[n+1][2];
+        int[] curr=new int[2];
+        int[] next=new int[2];
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 int profit=0;
                 if(buy==1){
-                    int buyKaro=dp[ind+1][0]-prices[ind];
-                    int skipKaro=dp[ind+1][1];
+                    int buyKaro=next[0]-prices[ind];
+                    int skipKaro=next[1];
                     profit=Math.max(buyKaro,skipKaro);
                 }else{
-                    int sellKaro=dp[ind+1][1]+prices[ind];
-                    int skipKaro=dp[ind+1][0];
+                    int sellKaro=next[1]+prices[ind];
+                    int skipKaro=next[0];
                     profit=Math.max(sellKaro,skipKaro);
                 }
-                dp[ind][buy]=profit;
+                curr[buy]=profit;
+                next=curr;
             }
         }
-        return dp[0][1];
+        return curr[1];
         //if buy=1 then buy karo else sell karo
     }
 }
