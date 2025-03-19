cca01b6ac62b06d497259c2895dbaf57124ba635
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-03-19 21:37:49 +0530

    Time: 3 ms (9.79%), Space: 48.9 MB (5.28%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        return getMaxProfit(prices,true,0);
-        //if buy=true then buy karo else sell karo
+        int n=prices.length;
+        int[][] dp=new int[n][2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return getMaxProfit(prices,1,0,dp);
+        //if buy=1 then buy karo else sell karo
     }
-    public int getMaxProfit(int[] prices,boolean buy,int ind){
+    public int getMaxProfit(int[] prices,int buy,int ind,int[][] dp){
         if(ind==prices.length)
          return 0;
+        if(dp[ind][buy]!=-1)
+         return dp[ind][buy];
         int profit=0;
-        if(buy){
-            int buyKaro=getMaxProfit(prices,false,ind+1)-prices[ind];
-            int skipKaro=getMaxProfit(prices,true,ind+1);
+        if(buy==1){
+            int buyKaro=getMaxProfit(prices,0,ind+1,dp)-prices[ind];
+            int skipKaro=getMaxProfit(prices,1,ind+1,dp);
             profit=Math.max(buyKaro,skipKaro);
         }else{
-            int sellKaro=getMaxProfit(prices,true,ind+1)+prices[ind];
-            int skipKaro=getMaxProfit(prices,false,ind+1);
+            int sellKaro=getMaxProfit(prices,1,ind+1,dp)+prices[ind];
+            int skipKaro=getMaxProfit(prices,0,ind+1,dp);
             profit=Math.max(sellKaro,skipKaro);
         }
-        return profit;
+         dp[ind][buy]=profit;
+         return profit;
     }
 }
