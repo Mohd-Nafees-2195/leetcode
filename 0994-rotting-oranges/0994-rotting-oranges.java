5eaafa9974000e667ea35715e1a0cac878843f2f
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-04-17 19:21:46 +0530

    Time: 1 ms (100%), Space: 42.7 MB (29.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,36 +3,34 @@ class Solution {
         Queue<int[]> q=new LinkedList<>();
         int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
         int n=grid.length,m=grid[0].length;
-        int[][] time=new int[n][m];
-        int ans=0;
+        int ans=-1,freshO=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
+                if(grid[i][j]==2)
                     q.add(new int[]{i,j});
-                    time[i][j]=0;
-                }
+                else if(grid[i][j]==1)
+                 freshO++; 
             }
         }
+        if(freshO==0) return 0;
         while(!q.isEmpty()){
-            int[] arr=q.poll();
-            for(int i=0;i<4;i++){
-                int x=arr[0]+dir[i][0];
-                int y=arr[1]+dir[i][1];
-                if(x>=0&&x<n&&y>=0&&y<m&&grid[x][y]==1){
-                    grid[x][y]=0;
-                    q.add(new int[]{x,y});
-                    time[x][y]+=time[arr[0]][arr[1]]+1;
+            int size=q.size();
+            ans++;
+            for(int i=0;i<size;i++){
+                int[] arr=q.poll();
+                for(int j=0;j<4;j++){
+                    int x=arr[0]+dir[j][0];
+                    int y=arr[1]+dir[j][1];
+                    if(x>=0&&x<n&&y>=0&&y<m&&grid[x][y]==1){
+                        grid[x][y]=0;
+                        q.add(new int[]{x,y});
+                        freshO--;
+                    }
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-                ans=Math.max(ans,time[i][j]);
-            }
-        }
+        if(freshO!=0)
+         return -1;
         return ans;
     }
 }
