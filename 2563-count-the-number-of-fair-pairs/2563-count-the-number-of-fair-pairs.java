73ae14998c539cda369ae7d1780662e05c8123a2
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-04-19 12:54:34 +0530

    Time: 63 ms (50.6%), Space: 57.2 MB (71.77%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -5,62 +5,54 @@ class Solution {
         long ans = 0; 
 
         for(int i=0;i<n;i++){
-            int l=lowerBound(nums,i+1,n-1,lower-nums[i]);
-            int r=upperBound(nums,i+1,n-1,upper-nums[i]);
+            int l=lowerBound(nums,i+1,n,lower-nums[i]);
+            int r=upperBound(nums,i+1,n,upper-nums[i]);
             ans+=(r-l);
         }
     
         return ans;
     }
-    public int lowerBound(int[] arr,int l,int r,int key){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]>=key) r=mid-1;
-            else l=mid+1;
-        }
-        return r;
-    }
-    public int upperBound(int[] arr,int l,int r,int key){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]<=key) l=mid+1;
-            else r=mid-1;
-        }
-        return r;
-    }
-
-
-
-
-
-
-
-
-
-
-
-    
-    // public static int lowerBound(int[] arr, int left, int right, int target) {
-    //     while (left < right) {
-    //         int mid = left + (right - left) / 2;
-    //         if (arr[mid] < target) {
-    //             left = mid + 1;
-    //         } else {
-    //             right = mid;
-    //         }
+    // public int lowerBound(int[] arr,int l,int r,int key){
+    //     while(l<=r){
+    //         int mid=l+(r-l)/2;
+    //         if(arr[mid]>=key) r=mid-1;
+    //         else l=mid+1;
     //     }
-    //     return left;
+    //     return r;
     // }
-    
-    // public static int upperBound(int[] arr, int left, int right, int target) {
-    //     while (left < right) {
-    //         int mid = left + (right - left) / 2;
-    //         if (arr[mid] <= target) {
-    //             left = mid + 1;
-    //         } else {
-    //             right = mid;
-    //         }
+    // public int upperBound(int[] arr,int l,int r,int key){
+    //     while(l<=r){
+    //         int mid=l+(r-l)/2;
+    //         if(arr[mid]<=key) l=mid+1;
+    //         else r=mid-1;
     //     }
-    //     return left;
+    //     return r;
     // }
+
+
+
+
+    public static int lowerBound(int[] arr, int left, int right, int target) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+    
+    public static int upperBound(int[] arr, int left, int right, int target) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
