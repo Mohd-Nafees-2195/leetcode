bbf41ca95178c4c2c667fe797e5cc82bdb218f7b
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-04-19 12:53:30 +0530

    Time: 66 ms (21.51%), Space: 57.5 MB (41.82%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -5,21 +5,40 @@ class Solution {
         long ans = 0; 
 
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int sum=nums[i]+nums[j];
-                if(sum>=lower&&sum<=upper) ans++;
-                else if(sum>upper) break;
-            }
+            int l=lowerBound(nums,i+1,n-1,lower-nums[i]);
+            int r=upperBound(nums,i+1,n-1,upper-nums[i]);
+            ans+=(r-l);
         }
-        
-        // for (int i = 0; i < n; i++) {
-        //     int l = lowerBound(nums, i + 1, n, lower - nums[i]);
-        //     int r = upperBound(nums, i + 1, n, upper - nums[i]);
-        //     ans += (r - l);
-        // }
-        
+    
         return ans;
     }
+    public int lowerBound(int[] arr,int l,int r,int key){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]>=key) r=mid-1;
+            else l=mid+1;
+        }
+        return r;
+    }
+    public int upperBound(int[] arr,int l,int r,int key){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(arr[mid]<=key) l=mid+1;
+            else r=mid-1;
+        }
+        return r;
+    }
+
+
+
+
+
+
+
+
+
+
+
     
     // public static int lowerBound(int[] arr, int left, int right, int target) {
     //     while (left < right) {
