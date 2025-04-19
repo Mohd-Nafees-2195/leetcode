494b1619cdb184fa07f9dc3cad0f8d948a137265
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-04-19 13:08:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -3,29 +3,14 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         long ans = 0; 
-
-        for(int i=0;i<n;i++){
-            int l=lowerBound(nums,i+1,n-1,lower-nums[i]);
-            int r=upperBound(nums,i+1,n-1,upper-nums[i]);
-            ans+=(r-l);
+        return bound(nums,0,n-1,upper)-bound(nums,0,n-1,lower-1);
+    }
+    public int bound(int[] arr,int l,int r,int key){
+        int ans=0;
+        while(l<=r){
+            if(arr[l]+arr[r]>key) r--;
+            else ans+=r-l++;
         }
-    
         return ans;
-    }
-    public int lowerBound(int[] arr,int l,int r,int key){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]>=key) r=mid-1;
-            else l=mid+1;
-        }
-        return r;
-    }
-    public int upperBound(int[] arr,int l,int r,int key){
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(arr[mid]<=key) l=mid+1;
-            else r=mid-1;
-        }
-        return r;
     }
 }
