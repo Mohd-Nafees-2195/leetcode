c7f351dc91982e9789b4203dc07eed305746de6c
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-04-23 21:39:55 +0530

    Time: 3 ms (95.34%), Space: 40.3 MB (90.68%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.java
+++ 1399-count-largest-group/1399-count-largest-group.java
@@ -1,13 +1,33 @@
 class Solution {
     public int countLargestGroup(int n) {
-        int[] arr=new int[5];
-        int f=10;
-        for(int i=1;i<5;i++){
-            arr[i]=f;
-            f=f*10;
+        int[] groupCounts = new int[37]; // Max digit sum for 9999 is 9+9+9+9=36
+
+        for (int i = 1; i <= n; i++) {
+            int sum = digitSum(i);
+            groupCounts[sum]++;
         }
-        int size=(n+"").length();
-        if(size==1) return n;
-        return n-(n/arr[size-1])*10+1;
+
+        int maxSize = 0;
+        for (int count : groupCounts) {
+            maxSize = Math.max(maxSize, count);
+        }
+
+        int result = 0;
+        for (int count : groupCounts) {
+            if (count == maxSize) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    private int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
 }
