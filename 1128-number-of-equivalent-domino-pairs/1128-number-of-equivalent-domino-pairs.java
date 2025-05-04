4ce2f63c1bb3a5e2fe756b7b358937701b156e38
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-05-04 12:10:23 +0530

    Time: 62 ms (8.4%), Space: 55.3 MB (29.52%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -6,8 +6,12 @@ class Solution {
             if(map.containsKey(arr[0]+""+arr[1])){
                 ans+=map.get(arr[0]+""+arr[1]);
             }
-            map.put(arr[0]+""+arr[1],map.getOrDefault(arr[0]+""+arr[1],0)+1);
-            map.put(arr[1]+""+arr[0],map.getOrDefault(arr[1]+""+arr[0],0)+1);
+            if(arr[0]!=arr[1]){
+                map.put(arr[0]+""+arr[1],map.getOrDefault(arr[0]+""+arr[1],0)+1);
+                map.put(arr[1]+""+arr[0],map.getOrDefault(arr[1]+""+arr[0],0)+1);
+            }else{
+                map.put(arr[0]+""+arr[1],map.getOrDefault(arr[0]+""+arr[1],0)+1);
+            }
         } 
         return ans;
     }
