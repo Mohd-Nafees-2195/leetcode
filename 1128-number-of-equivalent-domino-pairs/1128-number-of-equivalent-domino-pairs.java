62ee80d2510e7a5447a9adc0ac0fd3e1051cc492
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-05-04 10:50:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<String,Integer> map=new HashMap<>();
+        int ans=0;
+        for(int[] arr:dominoes){
+            if(map.containsKey(arr[0]+""+arr[1])){
+                ans+=map.get(arr[0]+""+arr[1]);
+            }
+            map.put(arr[0]+""+arr[1],map.getOrDefault(arr[0]+""+arr[1],0)+1);
+            map.put(arr[1]+""+arr[0],map.getOrDefault(arr[1]+""+arr[0],0)+1);
+        } 
+        return ans;
+    }
+}
