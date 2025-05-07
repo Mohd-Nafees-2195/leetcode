50afbad108b0b719ddb542785aa35405cdfff08d
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-05-07 09:37:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,20 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m=word1.length(),n=word2.length();
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return getMin(word1,word2,m-1,n-1,dp);
+       // int[][] dp=new int[m][n];
+        // for(int i=0;i<m;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        return getMin(word1,word2,m-1,n-1);
     }
-    public int getMin(String s1,String s2,int i,int j,int[][] dp){
-       
-        if(j<0) return i+1;
+    public int getMin(String s1,String s2,int i,int j){
         if(i<0) return j+1;
-        if(dp[i][j]==-1){
-            if(s1.charAt(i)==s2.charAt(j))
-             dp[i][j]=getMin(s1,s2,i-1,j-1,dp);
-            else{
-                 int insert = getMin(s1, s2, i, j - 1, dp);
-                int delete = getMin(s1, s2, i - 1, j, dp);
-                int replace = getMin(s1, s2, i - 1, j - 1, dp);
-                dp[i][j] = Math.min(insert, Math.min(delete, replace)) + 1;
-            }
-        }
-        return dp[i][j];
+        if(j<0) return i+1;
+        if(s1.charAt(i)==s2.charAt(j))
+            return getMin(s1,s2,i-1,j-1);
+        int insert = getMin(s1, s2, i, j - 1);
+        int delete = getMin(s1, s2, i - 1, j);
+        int replace = getMin(s1, s2, i - 1, j - 1);
+        return Math.min(insert, Math.min(delete, replace)) + 1;
     }
 }
