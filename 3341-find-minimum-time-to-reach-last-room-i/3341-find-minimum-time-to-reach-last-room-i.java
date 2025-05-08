a6d2c7a380994cc3197485ec3b5c9ad35304fd23
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-05-08 17:11:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
@@ -1,20 +1,30 @@
 class Solution {
-
     public int minTimeToReach(int[][] moveTime) {
-        PriorityQueue<int[]> queue = new PriorityQueue<>((o, p) -> o[0] - p[0]);
-        Integer[][] dp = new Integer[moveTime.length][moveTime[0].length];
-         queue.offer(new int[3]);
-        while (!queue.isEmpty()) {
-            int[] poll = queue.poll();
-           if (poll[1] == moveTime.length - 1 && poll[2] == moveTime[0].length - 1) {
-                return poll[0];
-            } else if (poll[1] >= 0 && poll[1] < moveTime.length && poll[2] >= 0 && poll[2] < moveTime[0].length && dp[poll[1]][poll[2]] == null) {
-                dp[poll[1]][poll[2]] = poll[0];
-                queue.offer(new int[] { Math.max(poll[0], poll[1] > 0 ? moveTime[poll[1] - 1][poll[2]] : 0) + 1, poll[1] - 1, poll[2] });
-                queue.offer(new int[] { Math.max(poll[0], poll[2] > 0 ? moveTime[poll[1]][poll[2] - 1] : 0) + 1, poll[1], poll[2] - 1 });
-                queue.offer(new int[] { Math.max(poll[0], poll[2] < moveTime[0].length - 1 ? moveTime[poll[1]][poll[2] + 1] : 0) + 1, poll[1], poll[2] + 1 });
-                queue.offer(new int[] { Math.max(poll[0], poll[1] < moveTime.length - 1 ? moveTime[poll[1] + 1][poll[2]] : 0) + 1, poll[1] + 1, poll[2] });
+        Queue<int[]> q=new PriorityQueue<>((p1,p2)->p1[0]-p2[0]);
+        int m=moveTime.length,n=moveTime[0].length;
+        int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int count=0;
+        q.add(new int[]{0,0,0});
+        while(!q.isEmpty()){
+            int[] arr=q.poll();
+            if(arr[1]==m-1&&arr[2]==n-1){
+                return arr[0];
+            }else if(dp[arr[1]][arr[2]]==-1){
+                dp[arr[1]][arr[2]]=arr[0];
+                for(int i=0;i<4;i++){
+                    int x=dir[i][0]+arr[1];
+                    int y=dir[i][1]+arr[2];
+                    if(x>=0&&x<m&&y>=0&&y<n){
+                        int max=Math.max(arr[0],moveTime[x][y]-arr[0]);
+                        q.add(new int[]{max+1,x,y});
+                    }
+                }
             }
+
         }
         return -1;
     }
