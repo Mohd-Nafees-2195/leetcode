5846586c99b2f0ebad24b3cc3c3564b0490dfdf9
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-06-19 18:38:34 +0530

    Time: 62 ms (9.38%), Space: 130.5 MB (5.4%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,84 +1,49 @@
 class LRUCache {
     int capacity;
-    Node head, tail;
-    Map<Integer, Node> cache;
-
+    Map<Integer,Node> map;
+    LinkedHashSet<Node> set;
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        cache = new HashMap<>();
+        this.capacity=capacity;
+        this.map=new HashMap<>();
+        this.set=new LinkedHashSet<>();
     }
-
+    
     public int get(int key) {
-        if (cache.containsKey(key)) {
-
-            Node temp = cache.get(key);
-            int value=temp.value;
-            delete(temp);
-            add(key,value);
-            return value;
-        } else
-            return -1;
-
+        if(map.containsKey(key)){
+            Node temp=map.get(key);
+            set.remove(temp);
+            set.add(temp);
+            return temp.value;
+        }
+        return -1;
     }
-
+    
     public void put(int key, int value) {
-         if (cache.containsKey(key)) {
-            delete(cache.get(key));
-            add(key,value);
-        } else if (cache.size() == capacity) {
-            delete(head);
-            add(key, value);
-        } else {
-             add(key, value);
-        }
-    }
-
-    public void delete(Node temp) {
-        cache.remove(temp.key);
-        if (head == tail) {
-            head = null;
-            tail = null;
-        } else if (temp == tail) {
-          tail=temp.prev;
-          temp.prev=null;
-          tail.next=null;
-        } else if (temp == head) {
-            head = head.next;
-            head.prev = null;
-            temp.next = null;
-        } else {
-            temp.prev.next = temp.next;
-            temp.next.prev = temp.prev;
-            temp.next = null;
-            temp.prev = null;
-        }
-    }
-
-    public void add(int key,int value) {
-        Node temp=new Node(key,value);
-        if(head==null){
-            head=temp;
-            tail=temp;
+        if(capacity==0) return;
+        if(map.containsKey(key)){
+            Node temp=map.get(key);
+            set.remove(temp);
+            temp.value=value;
+            set.add(temp);
+        }else if(map.size()<capacity){
+            Node temp=new Node(key,value);
+            map.put(key,temp);
+            set.add(temp);
         }else{
-        tail.next = temp;
-        temp.prev = tail;
-        tail = temp;
+            Node temp=set.iterator().next();
+            map.remove(temp.key);
+            set.remove(temp);
+            temp.key=key;temp.value=value;
+            map.put(key,temp);
+            set.add(temp);
         }
-       cache.put(key,temp);
     }
-}
-
-class Node {
-    int value;
-    int key;
-    Node next;
-    Node prev;
-
-    Node(int key, int value) {
-        this.value = value;
-        this.key = key;
-        this.next = null;
-        this.prev = null;
+    class Node{
+        int key,value;
+        Node(int key,int value){
+            this.key=key;
+            this.value=value;
+        }
     }
 }
 
