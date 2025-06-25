7c2fb0cb3b3b19c28d609580e33d6ec3b4fb2167
Mohd-Nafees-2195 <139738343+Mohd-Nafees-2195@users.noreply.github.com>
2025-06-25 20:48:27 +0530

    Time: 1169 ms (5.04%), Space: 55.9 MB (11.63%) - LeetHub

--- 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,21 +1,56 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        int n1=nums1.length,n2=nums2.length;
-        Queue<Long> q=new PriorityQueue<Long>((a,b)->(int)(b-a));
-        for(int i=0;i<n1;i++){
-            for(int j=0;j<n2;j++){
-                if(q.size()<k){
-                    q.add((long)nums1[i]*nums2[j]);
-                }else{
-                    if(q.peek()>=(long)nums1[i]*nums2[j]){
-                        q.poll();
-                        q.add((long)nums1[i]*nums2[j]);
-                    }else{
-                        if(nums1[i]>0&&nums2[j]>0) break;
-                    };
-                }
+        long l = -1_000_000_000L * 1_000_000_000L;
+        long h =  1_000_000_000L * 1_000_000_000L; 
+        long ans=0;
+        while(l<=h){
+            long midPro=l+(h-l)/2;
+            long cntSmallest=cntSmallest(nums1,nums2,midPro);
+            if(cntSmallest>=k){
+                ans=midPro;
+                h=midPro-1;
+            }else{
+                l=midPro+1;
             }
         }
-        return q.peek();
+        return ans;
     }
+    private long cntSmallest(int[] A, int[] B, long x) {
+        long count = 0;
+        int n = B.length;
+
+        for (int a : A) {
+            if (a > 0) {
+                int l = 0, r = n - 1, pos = -1;
+                while (l <= r) {
+                    int m = l + (r - l) / 2;
+                    if ((long) a * B[m] <= x) {
+                        pos = m;
+                        l = m + 1;
+                    } else {
+                        r = m - 1;
+                    }
+                }
+                count += pos + 1;
+            } else if (a < 0) {
+                int l = 0, r = n - 1, pos = n;
+                while (l <= r) {
+                    int m = l + (r - l) / 2;
+                    if ((long) a * B[m] <= x) {
+                        pos = m;
+                        r = m - 1;
+                    } else {
+                        l = m + 1;
+                    }
+                }
+                count += n - pos;
+            } else {
+                if (x >= 0)
+                    count += n;
+            }
+        }
+
+        return count;
+    }
+    
 }
